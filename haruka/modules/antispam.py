92722c9690790323cf52ec6db34d36b5d98d2fa4
ramitgangwar <43425662+ramitgangwar@users.noreply.github.com>
2019-09-25 11:06:36 +0530

    Update antispam.py

--- haruka/modules/antispam.py
+++ haruka/modules/antispam.py
@@ -96,7 +96,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
     ok123 = mention_html(user_chat.id, user_chat.first_name)
 
 
-    text12 = f"*Summoning all infinity stones* The end is near. ☠️ {ok123}."
+    text12 = f"*⚡️Another Bitch Goes Off⚡️* RIP {ok123}."
     update.effective_message.reply_text(text12, parse_mode=ParseMode.HTML)
 
     banner = update.effective_user  # type: Optional[User]
@@ -138,7 +138,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
                    "{} has been successfully gbanned!".format(mention_html(user_chat.id, user_chat.first_name)),
                    html=True)
-    text13 = f"Justice has been done with {ok123} 😉 'Peace'."
+    text13 = f"Successfully gbanned {ok123} 🙂 'He Deserve This'."
     update.effective_message.reply_text(text13, parse_mode=ParseMode.HTML)
 
 
