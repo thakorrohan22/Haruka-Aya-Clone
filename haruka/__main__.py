2551e4c91fd22d139bda6b397f21e88672ef5a1e
Freak-art <57123196+Freak-art@users.noreply.github.com>
2020-01-11 18:21:10 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -22,11 +22,11 @@ from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}!
 
-I'm here to help you manage your groups!. Need Any Help Join Our Support Group ( @MenheraChanSupport) 
+I'm here to help you manage your groups!. Need Any Help Join Our Support Group ( @CuratorSCrew) 
 
-I'm a group manager bot maintained by this [sexy boi](https://t.me/TheDarkW3b) I'm built in python3, using the python-telegram-bot library.
+I'm a group manager bot maintained by this [sexy boi](https://t.me/Divya_xd) I'm built in python3, using the python-telegram-bot library.
 
-Want to add me to your group? [Click here!](t.me/MenheraChanRoBot?startgroup=true)
+Want to add me to your group? [Click here!](t.me/shizuka_robot?startgroup=true)
 
 Click (Help) button to find out more about how to use me to my full potential."""
 
