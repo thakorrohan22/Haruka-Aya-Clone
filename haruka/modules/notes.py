a39ae7629ba76a05132552236198ce14bc0ad757
HACKERZKALI <62069925+HACKERZKALI@users.noreply.github.com>
2020-03-20 14:07:12 +0530

    Update notes.py

--- haruka/modules/notes.py
+++ haruka/modules/notes.py
@@ -232,7 +232,7 @@ def list_notes(bot: Bot, update: Update):
     note_list = sql.get_all_chat_notes(chat_id)
 
     for note in note_list:
-        note_name = " • `#{}`\n".format(note.name)
+        note_name = " • `{}`\n".format(note.name)
         if len(msg) + len(note_name) > MAX_MESSAGE_LENGTH:
             update.effective_message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
             msg = ""
