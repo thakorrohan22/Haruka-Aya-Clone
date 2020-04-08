4f4460418b231ef87a908d26c17ec72c48e7ab3c
technicalhackerz <62203070+technicalhackerz@users.noreply.github.com>
2020-04-08 10:06:32 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -20,7 +20,7 @@ from haruka.modules.helper_funcs.misc import paginate_modules
 from haruka.modules.translations.strings import tld, tld_help 
 from haruka.modules.connection import connected
 
-PM_START = """Hello {}, my name is {}!
+PM_START = """Hello {}, My Name is {}!
 
 You know how hard it is sometimes to manage group so here is the solution for you
 
