ed8112d27ab4e6be7ae4c80e511446502ef68043
thakorrohan22 <48714583+thakorrohan22@users.noreply.github.com>
2020-04-26 16:59:02 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -132,7 +132,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             send_start(bot, update)
     else:
-        update.effective_message.reply_text("I'm alive")
+        update.effective_message.reply_text("Hey Sir I'm Here")
 
 def send_start(bot, update):
     #Try to remove old message
