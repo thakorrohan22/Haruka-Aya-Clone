b91b924e1b6a75f4a3efaa385df768cbc1852c84
thakorrohan22 <48714583+thakorrohan22@users.noreply.github.com>
2020-04-29 17:13:17 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -26,8 +26,6 @@ Click Help button to find out more about how to use me to my full potential.
 
 Join My Support Group [Nancy MomoLand Support Group](http://t.me/nancy_Momoland_support_group) if you need any support or help.
 
-Made with love by [Unique](http://t.me/Unique_xD)
-
 Want to add me to your group? Click here! [Click Here](http://t.me/nancymomoland_PRobot?startgroup=true)
 """
 
